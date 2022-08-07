d02a1eb6c08e4c5eaced6061bc4e0c51114f171c
Thasyo Peres <Thasyotp2002@gmail.com>
2022-08-06 22:45:22 -0300

    second commit

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 
-import {useState, /*useEffect*/} from "react";
-
-//4- CUSTOM HOOK
-import {useFetch} from "./hooks/useFetch"
+import {useState, useEffect} from "react";
 
 const url = "http://localhost:3000/products"
 
@@ -12,25 +9,22 @@ function App() {
   //utilizando o useState para salvar os dados da API e manipulá-los.
   const [products, setProducts] = useState([])
 
-  //4- CUSTOM HOOK
-  const {data: items, httpConfig} = useFetch(url)
-
   //Criando os useStates dos input do form.
   const [name, setName] = useState("")
   const [price, setPrice] = useState("")
 
   //1- resgatando dados
-  //useEffect(() =>{
-    //async function fetchData() {
+  useEffect(() =>{
+    async function fetchData() {
 
-      //const res = await fetch(url); //Essa linha de código vai fazer a requisição dos dados, no formato json.
+      const res = await fetch(url); //Essa linha de código vai fazer a requisição dos dados, no formato json.
       
-      //const data = await res.json(); //Essa linha de código vai transformar os dados de json para object, afim de manipula-los no template.
+      const data = await res.json(); //Essa linha de código vai transformar os dados de json para object, afim de manipula-los no template.
     
-      //setProducts(data); //Recebendo e salvando os dados recebidos.
-    //}
-    //fetchData(); //fazendo a chamada da função
-  //}, [])
+      setProducts(data); //Recebendo e salvando os dados recebidos.
+    }
+    fetchData(); //fazendo a chamada da função
+  }, [])
 
   //2- Adicionando dados na API.
   const handleSubmit = async (e) => {
@@ -41,25 +35,22 @@ function App() {
       name,
       price,
     };
-    //console.log(product)
+    console.log(product)
 
     //Adicionando dados á API.
-    //const res = await fetch(url, {
-      //method: "POST", //POST: adiciona dados na API.
-      //headers: {
-        //"Content-Type": "application/json"
-      //},
-      //body: JSON.stringify(product)
-    //});
-    //console.log(res)
+    const res = await fetch(url, {
+      method: "POST", //POST: adiciona dados na API.
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    });
+    console.log(res)
 
     //3- Carregamento dinâmico.
-    //const addProduct = await res.json(); //Transformando json em object novamente.
+    const addProduct = await res.json(); //Transformando json em object novamente.
 
-    //setProducts((prevProducts) => [...prevProducts, addProduct]) //Tá fazendo a junção dos dados já escritos com os novos dados recebidos do form.
-
-    //5- REFATORANDO POST.
-    httpConfig(product, "POST")
+    setProducts((prevProducts) => [...prevProducts, addProduct]) //Tá fazendo a junção dos dados já escritos com os novos dados recebidos do form.
 
     //Limpando dados dos inputs após o envio dos mesmos.
     setName("")
@@ -71,8 +62,10 @@ function App() {
       <h1>Lista de Produtos</h1>
       {/* Exibindo os dados recebidos da API no front. */}
       <ul>
-        {items && items.map((product) => (
-          <li key={product.id}> {product.name} - R$ {product.price} </li>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
         ))}
       </ul>
 
